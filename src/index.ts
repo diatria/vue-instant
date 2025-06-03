9121af8852ba921947c818ba08a06812f406acfa
Dimas Adi Satria <dimss.satria@gmail.com>
2025-06-03 14:34:35 +0700

    add new container component

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import ComContainer from './components/ComContainer.vue';
 import ComDialogConfirmation from './components/ComDialogConfirmation.vue';
 import ComForm from './components/ComForm.vue';
 import ComSelect from './components/ComSelect.vue';
 import ComTable from './components/ComTable.vue';
 import * as helpers from './utils/helpers';
 
-export { ComDialogConfirmation, ComForm, ComSelect, ComTable, helpers };
+export { ComContainer, ComDialogConfirmation, ComForm, ComSelect, ComTable, helpers };
