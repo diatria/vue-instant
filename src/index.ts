d1dce65823dcf220be29502fcb5566ddce00d016
Dimas Adi Satria <dimss.satria@gmail.com>
2025-06-05 13:50:58 +0700

    add export runtimeConfig

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ import ComForm from './components/ComForm.vue';
 import ComSelect from './components/ComSelect.vue';
 import ComTable from './components/ComTable.vue';
 import * as helpers from './utils/helpers';
+import * as runtimeConfig from './config/runtimeConfig'
 
-export { ComContainer, ComDialogConfirmation, ComForm, ComSelect, ComTable, helpers };
+export { ComContainer, ComDialogConfirmation, ComForm, ComSelect, ComTable, helpers, runtimeConfig };
