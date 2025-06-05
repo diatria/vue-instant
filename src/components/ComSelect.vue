118203096a9b1bfe81765647091c3946d4975296
Dimas Adi Satria <dimss.satria@gmail.com>
2025-06-05 09:07:38 +0700

    feat: add remote search in select

--- src/components/ComSelect.vue
+++ src/components/ComSelect.vue
@@ -1,54 +1,109 @@
 <script lang="ts" setup>
-  import { httpGet } from '../utils/helpers';
-  import { get } from 'lodash';
-  import { computed, onMounted, ref } from 'vue';
+  import { httpGet } from '../utils/helpers'
+  import { get } from 'lodash'
+  import { computed, onMounted, ref } from 'vue'
 
-  const emit = defineEmits(['update:modelValue']);
-  const props = defineProps({
-    url: { type: String, required: false },
-    fieldLabel: { type: String, required: false },
-    fieldValue: { type: String, required: false },
-    options: { type: Array<unknown>, required: false },
-    autoFetch: { type: Boolean, required: false, default: true },
-    disabled: { type: Boolean, required: false, default: false },
-  });
+  interface RemoteSearch {
+    field?: string
+    strict?: boolean
+  }
 
-  const collections = ref<Array<Record<string, never>>>([]);
-  const value = ref('');
+  const emit = defineEmits(['update:modelValue'])
+  const props = defineProps<{
+    url?: string
+    remoteSearch?: RemoteSearch
+    fieldLabel?: string
+    fieldValue?: string
+    options?: unknown[]
+    autoFetch?: boolean
+    disabled?: boolean
+    remote?: boolean
+  }>()
 
-  const fieldLabel = computed(() => props.fieldLabel ?? 'name');
-  const fieldValue = computed(() => props.fieldValue ?? 'id');
+  const collections = ref<Array<Record<string, never>>>([])
+  const value = ref('')
+
+  const fieldLabel = computed(() => props.fieldLabel ?? 'name')
+  const fieldValue = computed(() => props.fieldValue ?? 'id')
 
   // Methods
   function fetchingDataFromServer() {
     httpGet(`${import.meta.env.VITE_API_URL}/${props.url}`).then(result => {
-      collections.value = result.data.data;
-    });
+      collections.value = result.data.data
+    })
+  }
+
+  function fetchingDataFromServerWithFilter(searchText: string) {
+    httpGet(`${import.meta.env.VITE_API_URL}/${props.url}`, {
+      params: {
+        queries: [
+          {
+            field: props.remoteSearch?.field,
+            value: searchText,
+            strict: props.remoteSearch?.strict ?? false,
+          },
+        ],
+        limit: 10,
+      },
+    }).then(result => {
+      collections.value = result.data.data
+    })
   }
 
   onMounted(() => {
-    if (props.url && props.autoFetch) fetchingDataFromServer();
-    if (props.options) collections.value = props.options as Array<Record<string, never>>;
-  });
+    if (props.url && props.autoFetch) fetchingDataFromServer()
+    if (props.options) collections.value = props.options as Array<Record<string, never>>
+  })
 
   defineExpose({
     fetchingDataFromServer,
-  });
+  })
 </script>
 
 <template>
-   <!-- Type select untuk fetch data ke API --> <el-select
+
+  <!-- Remote search -->
+
+  <el-select
+    v-model="value"
+    v-if="props.remote"
+    :disabled="props.disabled"
+    :remote-method="fetchingDataFromServerWithFilter"
+    @change="(val: any) => emit('update:modelValue', val)"
+    placeholder="Select"
+    remote
+    filterable
+    remote-show-suffix
+  >
+
+    <el-option
+      v-for="item in collections"
+      :key="item[fieldValue]"
+      :label="get(item, fieldLabel)"
+      :value="item[fieldValue] ?? item['id']"
+    />
+
+  </el-select>
+
+  <!-- Type select untuk fetch data ke API -->
+
+  <el-select
+    v-else
     v-model="value"
     :disabled="props.disabled"
     @change="(val: any) => emit('update:modelValue', val)"
     placeholder="Select"
     filterable
-    > <el-option
+  >
+
+    <el-option
       v-for="item in collections"
       :key="item[fieldValue]"
       :label="get(item, fieldLabel)"
       :value="item[fieldValue] ?? item['id']"
-    /> </el-select
-  >
+    />
+
+  </el-select>
+
 </template>
 
