96ff1de3cacd6423157c3a3155650f0481a2d4e2
Ubuntu PMLI <dimss.satria@gmail.com>
2025-09-01 16:04:03 +0700

    feat(ComTable): Add delete button to ComTable component

--- src/components/ComTable.vue
+++ src/components/ComTable.vue
@@ -3,10 +3,11 @@ import type { Query } from '../types'
 import type { RouteLocationRaw } from 'vue-router'
 import { onMounted, reactive, ref } from 'vue'
 import { httpDelete, httpGet, httpHandleError } from '../utils/helpers'
-import { Edit, MoreFilled, View } from '@element-plus/icons-vue'
+import { Delete, Edit, MoreFilled, Plus, View } from '@element-plus/icons-vue'
 
 const emits = defineEmits(['tableSelections'])
 const props = defineProps<{
+  buttonCreateUrl?: () => RouteLocationRaw;
   buttonEditUrl?: (row: { id: number | string }) => RouteLocationRaw
   buttonViewUrl?: (row: { id: number | string }) => RouteLocationRaw
   columns: Array<{
@@ -117,80 +118,60 @@ defineExpose({ refresh, remove })
 <template>
   <div>
     <!-- Toolbar -->
-    <div class="flex justify-end">
-      <el-button v-if="!$slots.buttonDelete" @click="dialogDeleteConfirmation = true" type="danger"
-        >Hapus</el-button
-      >
+    <div class="flex justify-end gap-4 pb-4 border-b border-[#ebeef5]">
+      <RouterLink v-if="props.buttonCreateUrl" :to="props.buttonCreateUrl()">
+        <el-button :icon="Plus" type="primary">Tambah</el-button>
+      </RouterLink>
+      <el-button v-if="!$slots.buttonDelete && dataSelected.length" @click="dialogDeleteConfirmation = true" :icon="Delete" type="danger" class="!m-0">Hapus</el-button>
       <slot name="buttonDelete"></slot>
-      <el-button>Filter</el-button>
+      <el-button class="!m-0">Filter</el-button>
     </div>
 
     <!-- Table -->
-    <el-table
-      v-loading="loading"
-      :data="data"
-      @selection-change="handleSelectionChange"
-      style="width: 100%"
-    >
+    <el-table v-loading="loading" :data="data" @selection-change="handleSelectionChange" style="width: 100%">
       <el-table-column type="selection" width="55" fixed="left" />
       <template v-for="(column, index) of props.columns" :key="index">
-        <el-table-column
-          v-if="!column.value && column.type !== 'slot'"
-          :prop="column.field"
-          :label="column.label"
-          :width="column.width"
-          :align="column.align ?? 'left'"
-        />
+        <el-table-column v-if="!column.value && column.type !== 'slot'" :prop="column.field" :label="column.label"
+          :width="column.width" :align="column.align ?? 'left'" />
         <!-- Custom value -->
-        <el-table-column
-          v-if="column.value && column.type !== 'slot'"
-          :prop="column.field"
-          :label="column.label"
-          :width="column.width"
-          :align="column.align ?? 'left'"
-        >
+        <el-table-column v-if="column.value && column.type !== 'slot'" :prop="column.field" :label="column.label"
+          :width="column.width" :align="column.align ?? 'left'">
           <template #default="scope">{{
             typeof column.value === 'function' ? column.value(scope.row) : ''
-          }}</template>
+            }}</template>
         </el-table-column>
         <!-- Slot -->
-        <el-table-column
-          v-if="column.type === 'slot'"
-          :prop="column.field"
-          :label="column.label"
-          :width="column.width"
-          :align="column.align ?? 'left'"
-        >
-          <template #default="scope"><slot :name="column.field" :row="scope.row" /></template>
+        <el-table-column v-if="column.type === 'slot'" :prop="column.field" :label="column.label" :width="column.width"
+          :align="column.align ?? 'left'">
+          <template #default="scope">
+            <slot :name="column.field" :row="scope.row" />
+          </template>
         </el-table-column>
       </template>
       <el-table-column width="55" fixed="right">
         <template #default="scope">
-          <div
-            class="hover:cursor-pointer hover:bg-slate-200 justify-center rounded flex items-center"
-          >
+          <div class="hover:cursor-pointer hover:bg-slate-200 justify-center rounded flex items-center">
             <el-popover placement="bottom" :width="150" popper-class="!p-0" trigger="click">
               <template #reference>
-                <el-icon><MoreFilled /></el-icon>
+                <el-icon>
+                  <MoreFilled />
+                </el-icon>
               </template>
               <ul>
                 <!-- Action Button View -->
-                <RouterLink
-                  v-if="typeof buttonViewUrl === 'function'"
-                  :to="buttonViewUrl(scope.row)"
-                  target="_blank"
-                >
+                <RouterLink v-if="typeof buttonViewUrl === 'function'" :to="buttonViewUrl(scope.row)" target="_blank">
                   <li class="flex items-center py-2 px-4 hover:cursor-pointer hover:bg-slate-100">
-                    <el-icon><View /></el-icon> <span class="ml-2">Lihat</span>
+                    <el-icon>
+                      <View />
+                    </el-icon> <span class="ml-2">Lihat</span>
                   </li>
                 </RouterLink>
                 <!-- Action Button Edit -->
-                <RouterLink
-                  v-if="typeof buttonEditUrl === 'function'"
-                  :to="buttonEditUrl(scope.row)"
-                >
+                <RouterLink v-if="typeof buttonEditUrl === 'function'" :to="buttonEditUrl(scope.row)">
                   <li class="flex items-center py-2 px-4 hover:cursor-pointer hover:bg-slate-100">
-                    <el-icon><Edit /></el-icon> <span class="ml-2">Ubah</span>
+                    <el-icon>
+                      <Edit />
+                    </el-icon> <span class="ml-2">Ubah</span>
                   </li>
                 </RouterLink>
                 <slot name="action" :row="scope.row"></slot>
@@ -203,14 +184,8 @@ defineExpose({ refresh, remove })
 
     <!-- Pagination -->
     <div class="flex justify-end mt-4">
-      <el-pagination
-        v-model:page-size="pageSize"
-        v-model:current-page="currentPage"
-        :total="totalData"
-        :page-sizes="[10, 25, 50, 75, 100]"
-        @change="changePage"
-        layout="sizes, total, prev, pager, next"
-      />
+      <el-pagination v-model:page-size="pageSize" v-model:current-page="currentPage" :total="totalData"
+        :page-sizes="[10, 25, 50, 75, 100]" @change="changePage" layout="sizes, total, prev, pager, next" />
     </div>
 
     <!-- Dialog delete confirmation -->
