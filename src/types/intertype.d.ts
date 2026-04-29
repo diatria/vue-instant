357466798813bd0b8e9d5ddfccac5bcb507b2960
Dimas Adi Satria <dimss.satria@gmail.com>
2026-04-29 11:16:28 +0700

    change type for ComSelectProps.placement

--- src/types/intertype.d.ts
+++ src/types/intertype.d.ts
@@ -22,7 +22,7 @@ export interface ComSelectProps {
   field_value?: string
   field_search_column?: string
   placeholder?: string
-  placement?: string
+  placement?: 'top' | 'top-start' | 'top-end' | 'bottom' | 'bottom-start' | 'bottom-end' | 'left' | 'left-start' | 'left-end' | 'right' | 'right-start' | 'right-end'
   remote?: boolean
 }
 
